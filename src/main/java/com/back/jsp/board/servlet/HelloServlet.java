17ab9950829e38bf4142e9439792a7c03de2558d
Yoepee <kdy2975@naver.com>
2025-08-07 12:28:20 +0900

    auto setting

--- src/main/java/com/back/jsp/board/servlet/HelloServlet.java
+++ src/main/java/com/back/jsp/board/servlet/HelloServlet.java
@@ -8,15 +8,15 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/hello")
+@WebServlet("/hello") // 이 서블릿은 /hello 경로로 접근할 때 실행됨
 public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setCharacterEncoding( "UTF-8" ); // 들어오는 데이터를 UTF-8로 인코딩
+        req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8로 인코딩
         resp.setCharacterEncoding("UTF-8"); // 응답 데이터를 UTF-8로 인코딩
 
-        resp.setContentType("text/html; charset=UTF-8"); // 응답의 콘텐츠 타입을 HTML로 설정 - 브라우저한테 우리가 만든 결과물은 UTF-8로 인코딩된 HTML이라고 알려줌
+        resp.setContentType("text/html; charset=UTF-8"); // 브라우저한테 우리가 만든 결과물은 UTF-8로 인코딩된 HTML임을 알려줌
 
-        resp.getWriter().append("<h1>Hello, World!</h1>"); // 응답에 HTML 콘텐츠를 작성
+        resp.getWriter().append("<h1>Hello, World!555</h1>");
     }
 }
